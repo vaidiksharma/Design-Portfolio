61161382f7b9d1299ddaa99b23ab0078756c7ad1
Vaidik Sharma <vaidiksharma0@gmail.com>
2024-04-17 16:39:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,6 +24,34 @@ playBtn.addEventListener(
   false
 );
 
+document
+  .querySelector(".menu-button")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default behavior of the anchor tag
+    showSidebar();
+  });
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+  // Prevent scrolling on the body when the sidebar is open
+  document.body.style.overflow = "hidden";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+  // Restore scrolling on the body when the sidebar is closed
+  document.body.style.overflow = "auto";
+}
+
+// Automatically close the sidebar when a list item is clicked
+document.querySelectorAll(".sidebar a").forEach((item) => {
+  item.addEventListener("click", () => {
+    hideSidebar();
+  });
+});
+
 // resetBtn.addEventListener(
 //   "mouseover",
 //   function () {
